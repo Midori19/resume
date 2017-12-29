8c2d330485bbc33e744de820787d25eca971b5e1
Shinkai <34812566+Midori19@users.noreply.github.com>
2017-12-29 21:54:53 +0800

    Update

--- 04_untitled/js/main.js
+++ 04_untitled/js/main.js
@@ -6,12 +6,10 @@ $(function() {
 		var sliderBlock = $('.sec').offset().top + winH
 		if (winScr >= sliderBlock) {
 			$('.third .img .block').css('left','-105%')
-			console.log('run')
 		} else {
 			$('.third .img .block').css('left','0')
 		}
 
-		//opacity slider
 		var opacityVar = 1 - (winScr/winH)
 		opacityVar = opacityVar * 1.3
 		if (opacityVar >= 1) { opacityVar = 1 }
@@ -19,5 +17,10 @@ $(function() {
 
 		var sliderHeight = 50 + (50 * (winScr / $('.full').height()))
 		$('.one .vertical-align').css('top', sliderHeight + '%')
+
+		var moveImg = -150 + (80 * (winScr / $(window).height()))
+		$('.fourth img').css('transform', 'translate(-' + moveImg + 'px)')
+
+
 	});
 })
