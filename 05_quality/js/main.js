178d99a64d4d9ecbc4605489320a7a1fdcfcfdd9
Midori <plus6ultra@gmail.com>
2018-01-16 11:09:54 +0800

    modify

--- 05_quality/js/main.js
+++ 05_quality/js/main.js
@@ -1,5 +1,5 @@
 var jsonData
-var dataUrl = 'http://shinkai.ga/command.php'
+var dataUrl = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314'
 $.ajax({
     url: dataUrl,
     success: function(res) {
